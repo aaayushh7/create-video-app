88299292cfc5fad646a7cecd28f83fc74e5b13aa
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-01-28 22:19:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,7 @@ function App() {
   return (
     
         
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       
   );
 }
 
