50a0e01a6c02ec3df3f0e003c0b9997507b7804b
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-01-28 22:19:06 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
