938b2870fb2870cecec817e70717693c67eb26e4
humkobhiseekhnahai <113964544+humkobhiseekhnahai@users.noreply.github.com>
2024-02-01 17:06:35 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,154 @@
+// const { Server } = require("socket.io");
+
+// const io = new Server(8000, {
+//   cors: true,
+// });
+
+// const emailToSocketIdMap = new Map();
+// const socketidToEmailMap = new Map();
+
+// io.on("connection", (socket) => {
+//   console.log(`Socket Connected`, socket.id);
+//   socket.on("room:join", (data) => {
+//     const { email, room } = data;
+//     emailToSocketIdMap.set(email, socket.id);
+//     socketidToEmailMap.set(socket.id, email);
+//     io.to(room).emit("user:joined", { email, id: socket.id });
+//     socket.join(room);
+//     io.to(socket.id).emit("room:join", data);
+//   });
+
+//   socket.on("user:call", ({ to, offer }) => {
+//     io.to(to).emit("incomming:call", { from: socket.id, offer });
+//   });
+
+//   socket.on("call:accepted", ({ to, ans }) => {
+//     io.to(to).emit("call:accepted", { from: socket.id, ans });
+//   });
+
+//   socket.on("peer:nego:needed", ({ to, offer }) => {
+//     console.log("peer:nego:needed", offer);
+//     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+//   });
+
+//   socket.on("peer:nego:done", ({ to, ans }) => {
+//     console.log("peer:nego:done", ans);
+//     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+//   });
+// });
+
+// const { Server } = require("socket.io");
+
+// const io = new Server(8000, {
+//   cors: true,
+// });
+
+// const socketIdToEmailMap = new Map();
+
+// io.on("connection", (socket) => {
+//   console.log(`Socket Connected`, socket.id);
+//   socket.on("room:join", (data) => {
+//     const { email, room } = data;
+//     socketIdToEmailMap.set(socket.id, email);
+//     io.to(room).emit("user:joined", { email: socketIdToEmailMap.get(socket.id), id: socket.id });
+//     socket.join(room);
+//     io.to(socket.id).emit("room:join", data);
+//   });
+
+//   socket.on("user:call", ({ to, offer }) => {
+//     const toEmail = socketIdToEmailMap.get(to);
+//     if (toEmail) {
+//       io.to(to).emit("incomming:call", { from: socket.id, offer });
+//     }
+//   });
+
+//   socket.on("call:accepted", ({ to, ans }) => {
+//     const toEmail = socketIdToEmailMap.get(to);
+//     if (toEmail) {
+//       io.to(to).emit("call:accepted", { from: socket.id, ans });
+//     }
+//   });
+
+//   socket.on("peer:nego:needed", ({ to, offer }) => {
+//     console.log("peer:nego:needed", offer);
+//     const toEmail = socketIdToEmailMap.get(to);
+//     if (toEmail) {
+//       io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+//     }
+//   });
+
+//   socket.on("peer:nego:done", ({ to, ans }) => {
+//     console.log("peer:nego:done", ans);
+//     const toEmail = socketIdToEmailMap.get(to);
+//     if (toEmail) {
+//       io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+//     }
+//   });
+
+//   socket.on("disconnect", () => {
+//     const email = socketIdToEmailMap.get(socket.id);
+//     if (email) {
+//       socketIdToEmailMap.delete(socket.id);
+//     }
+//   });
+// });
+
+
+
+const { Server } = require("socket.io");
+
+const io = new Server(8000, {
+  cors: true,
+  origin: "http://192.168.238.247:3002",
+});
+
+const socketIdToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    socketIdToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email: socketIdToEmailMap.get(socket.id), id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    const toEmail = socketIdToEmailMap.get(to);
+    if (toEmail) {
+      io.to(to).emit("incomming:call", { from: socket.id, offer });
+    }
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    const toEmail = socketIdToEmailMap.get(to);
+    if (toEmail) {
+      io.to(to).emit("call:accepted", { from: socket.id, ans });
+    }
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    const toEmail = socketIdToEmailMap.get(to);
+    if (toEmail) {
+      io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+    }
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    const toEmail = socketIdToEmailMap.get(to);
+    if (toEmail) {
+      io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const email = socketIdToEmailMap.get(socket.id);
+    if (email) {
+      socketIdToEmailMap.delete(socket.id);
+    }
+  });
+});
+
