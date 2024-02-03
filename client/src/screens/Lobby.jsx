c146fd3e0dac408f07e7c37aadd84870c222010a
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-03 18:37:56 +0530

    Update Lobby.jsx

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,69 +1,10 @@
-// import React, { useState, useCallback, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { useSocket } from "../context/SocketProvider";
-
-// const LobbyScreen = () => {
-//   const [email, setEmail] = useState("");
-//   const [room, setRoom] = useState("");
-
-//   const socket = useSocket();
-//   const navigate = useNavigate();
-
-//   const handleSubmitForm = useCallback(
-//     (e) => {
-//       e.preventDefault();
-//       socket.emit("room:join", { email, room });
-//     },
-//     [email, room, socket]
-//   );
-
-//   const handleJoinRoom = useCallback(
-//     (data) => {
-//       const { email, room } = data;
-//       navigate(`/room/${room}`);
-//     },
-//     [navigate]
-//   );
-
-//   useEffect(() => {
-//     socket.on("room:join", handleJoinRoom);
-//     return () => {
-//       socket.off("room:join", handleJoinRoom);
-//     };
-//   }, [socket, handleJoinRoom]);
-
-//   return (
-//     <div>
-//       <h1>Lobby</h1>
-//       <form onSubmit={handleSubmitForm}>
-//         <label htmlFor="email">Email ID </label>
-//         <input
-//           type="email"
-//           id="email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <br />
-//         <label htmlFor="room">Room Number </label>
-//         <input
-//           type="text"
-//           id="room"
-//           value={room}
-//           onChange={(e) => setRoom(e.target.value)}
-//         />
-//         <br />
-//         <button>Join</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default LobbyScreen;
-
-
 import React, { useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../context/SocketProvider";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const LobbyScreen = () => {
   const [email, setEmail] = useState("");
@@ -96,29 +37,60 @@ const LobbyScreen = () => {
   }, [socket, handleJoinRoom]);
 
   return (
-    <div>
-      <h1>Lobby</h1>
-      <form onSubmit={handleSubmitForm}>
-        <label htmlFor="email">Email ID </label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br></br>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#EBD9B4",
+      }}
+    >
+      <h1 style={{ marginBottom: "2rem" }}>LOBBY</h1>
+      <Box
+        component="form"
+        onSubmit={handleSubmitForm}
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <label htmlFor="email">Email ID</label>
+          <br></br>
+          <TextField
+            required
+            id="filled-required"
+            label="Required"
+            variant="filled"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br></br>
+          <br />
+          <label htmlFor="room">Room Number</label>
+          <br></br>
+          <div>
+            <TextField
+              required
+              id="filled-required"
+              label="Required"
+              variant="filled"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+            />
+          </div>
+        </div>
         <br />
-        <label htmlFor="room">Room Number </label>
-        <input
-          type="text"
-          id="room"
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <br />
         <br></br>
-        <button>Join</button>
-      </form>
+        <Stack direction="row" spacing={2} justifyContent={"center"}>
+          <Button variant="contained" type="submit">
+            JOIN
+          </Button>
+        </Stack>
+      </Box>
     </div>
   );
 };
